a770777760bf5d358866cbd73dd057b3ca04051c
Reno Qiu <dechao.qiu@gmail.com>
2014-03-17 16:07:17 -0700

    add c extension

---
+++ ext/atosl/extconf.rb
@@ -0,0 +1,9 @@
+require "mkmf"
+
+extension_name = 'atosl'
+
+abort "missing malloc()" unless have_func "malloc"
+abort "missing free()"   unless have_func "free"
+
+dir_config(extension_name)
+create_makefile(extension_name)

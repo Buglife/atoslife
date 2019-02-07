ca09fb48a7baf8d560a8a06fd03f3c7f248fb6a6
Dave Schukin <daveschukin@gmail.com>
2019-02-07 14:34:08 -0800

    Upgraded atosl + update extconf.rb to compile

--- ext/atosl/extconf.rb
+++ ext/atosl/extconf.rb
@@ -1,9 +1,60 @@
-require "mkmf"
+require 'mkmf'
 
 extension_name = 'atosl/atosl'
 
 abort "missing malloc()" unless have_func "malloc"
 abort "missing free()"   unless have_func "free"
+
+
+LIBDWARF_TARBALL_FILENAME = 'libdwarf_feb_7_2019.tar.gz'
+CWD = File.expand_path(File.dirname(__FILE__))
+
+def sys(cmd)
+  puts "  -- #{cmd}"
+  unless ret = xsystem(cmd)
+    raise "#{cmd} failed, please check #{CWD}/mkmf.log"
+  end
+  ret
+end
+
+puts "Current working directory: #{CWD}"
+
+$LIBPATH.unshift "#{CWD}/dst/lib"
+$INCFLAGS[0,0] = "-I#{CWD}/dst/include "
+
+puts "ℹ️  Installing libdwarf..."
+sys("tar zxvf #{LIBDWARF_TARBALL_FILENAME}")
+
+Dir.chdir('libdwarf') do
+	DIR_DST = "#{CWD}/dst"
+	DIR_DST_LIB = "#{CWD}/dst/lib"
+	DIR_DST_INCLUDE = "#{CWD}/dst/include"
+
+	ENV['CFLAGS'] = "-fPIC -I#{CWD}/dst/include"
+    ENV['LDFLAGS'] = "-L#{CWD}/dst/lib"
+	sys('./configure')
+	sys('make')
+	sys('make install')
+
+	[DIR_DST, DIR_DST_LIB, DIR_DST_INCLUDE].each do |dir_path|
+		file_path = "#{dir_path}/"
+		puts "• Checking if dir exists: #{file_path}"
+		unless File.directory?(file_path)
+			FileUtils.mkdir_p(file_path)
+			puts "  • Created directory: #{file_path}"
+		end
+	end
+	
+	FileUtils.cp 'libdwarf/.libs/libdwarf.a', "#{DIR_DST_LIB}/libdwarf_ext.a"
+	FileUtils.cp 'libdwarf/dwarf.h', "#{DIR_DST_INCLUDE}/"
+	FileUtils.cp 'libdwarf/libdwarf.h', "#{DIR_DST_INCLUDE}/"
+
+	unless have_library('dwarf_ext')
+	    raise 'libdwarf build failed'
+	  end
+end
+
+puts "✅  libdwarf installed"
 
 dir_config(extension_name)
 create_makefile(extension_name)
